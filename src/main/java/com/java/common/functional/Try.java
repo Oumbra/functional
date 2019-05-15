ae0b160b31c04d7c0f1d01e60afc84bb6ad09426
Damien Amoury <damien.amoury@sogelink.fr>
2019-05-15 17:26:40 +0200

    Simplification du système de chainage conditionel

--- src/main/java/com/java/common/functional/Try.java
+++ src/main/java/com/java/common/functional/Try.java
@@ -15,164 +15,185 @@ public interface Try<E> {
 
     /**
      * Retourne le type de l'execution
+     *
      * @return {@link com.java.common.functional.enums.TryType#SUCCESS} ou {@link com.java.common.functional.enums.TryType#FAILURE}
      */
     TryType getType();
 
     /**
      * Vérifie si le résultat ne contient aucune donnée
+     *
      * @return
      */
     boolean isEmpty();
 
     /**
      * Verifie si l'execution s'est déroulé sans erreur
+     *
      * @return
      */
     boolean isSuccess();
 
     /**
      * Verifie si l'execution a rencontrer une erreur
+     *
      * @return
      */
     boolean isFailure();
 
     /**
      * Cast l'instance Try en instance de {@link Success}
+     *
      * @return
      */
     Success<E> asSuccess();
 
     /**
      * Cast l'instance Try en instance de {@link Failure}
+     *
      * @return
      */
     Failure<E> asFailure();
 
     /**
      * Retourne la valeur potentielle ou Option.empty()
+     *
      * @return
      */
     Optional<E> toOption();
 
     /**
      * Retourne la valeur si aucune erreur n'a été rencontré ou la valeur par defaut
+     *
      * @param defaultValue Valeur par defaut
+     *
      * @return
      */
     E getOrElse(E defaultValue);
 
     /**
      * Retourne la valeur si aucune erreur n'a été rencontré ou souleve l'erreur
+     *
      * @return
+     *
      * @throws Exception
      */
     E getOrThrow() throws Exception;
 
     /**
-     * Retourne la valeur potentielle si aucune erreur n'a été rencontré ou souleve l'erreur
+     * Retourne la valeur si aucune erreur n'a été rencontré ou souleve l'erreur
+     *
+     * @param transformer Transforme l'exception en un type spécifié
+     *
      * @return
+     *
+     * @throws Exception
+     */
+    <ET extends Throwable> E getOrThrow(Function<Exception, ET> transformer) throws ET;
+
+    /**
+     * Retourne la valeur potentielle si aucune erreur n'a été rencontré ou souleve l'erreur
+     *
+     * @return
+     *
      * @throws Exception
      */
     Optional<E> getOptionOrThrow() throws Exception;
 
     /**
-     * Execute une lambda de type Consumer avec la valeur en entrée si aucune erreur n'a été rencontré
+     * Retourne la valeur potentielle si aucune erreur n'a été rencontré ou souleve l'erreur
+     *
+     * @param transformer Transforme l'exception en un type spécifié
+     *
+     * @return
+     *
+     * @throws ET
+     */
+    <ET extends Throwable> Optional<E> getOptionOrThrow(Function<Exception, ET> transformer) throws ET;
+
+    /**
+     * Si l'instance est de type SUCCESS: execute une lambda avec la valeur de l'instance
+     *
      * @param consumer Lambda de type Consumer
      */
     void ifPresent(Consumer<E> consumer);
 
+    /**
+     * Si l'instance est de type SUCCESS: execute une lambda avec la valeur de l'instance avant de renvoyer l'instance courante
+     *
+     * @param consumer Lambda de type Consumer
+     *
+     * @return Instance courante
+     */
     Try<E> peekIfPresent(Consumer<E> consumer);
 
+    /**
+     * Si l'instance est de type FAILURE: execute une lambda avec l'erreur de l'instance
+     *
+     * @param consumer Lambda de type Consumer
+     */
     void ifAbsent(Consumer<Exception> consumer);
 
+    /**
+     * Si l'instance est de type FAILURE: execute une lambda avec la valeur de l'instance avant de renvoyer l'instance courante
+     *
+     * @param consumer Lambda de type Consumer
+     *
+     * @return Instance courante
+     */
     Try<E> peekIfAbsent(Consumer<Exception> consumer);
 
     /**
      * Applique un filtre sur la valeur encapsulee
+     *
      * @param predicate Contrainte à appliquer
+     *
      * @return Retourne l'instance en cours si la contrainte est valide ou si l'instance est une Failure; dans le cas contraire, renvoi une instance vierge
      */
     Try<E> filter(Predicate<E> predicate);
 
     /**
-     * Applique un filtre sur la valeur encapsulee et si celui-ci est valide; execute et retourne le résultat du transformateur
-     * @param predicate Contrainte à appliquer sur la valeur de l'instance courante
+     * Applique un filtre sur l'instance et si celui-ci est valide; execute et retourne la valeur résultant de la transformation
+     *
+     * @param predicate   Contrainte à appliquer sur l'instance courante
      * @param transformer Lambda de transformation, les exceptions sont gérées
-     * @return Valeur encapsulé de la lambda de transformation, l'instance courante qui est une erreur ou bien l'erreur encontree au sein de la lambda de transformation
+     *
+     * @return le résultat de la lambda de transformation (valeur ou erreur) ou bien l'instance courante
      */
-    Try<E> when(Predicate<E> predicate, FunctionThrowable<E, E> transformer);
+    Try<E> when(Predicate<Try<E>> predicate, FunctionThrowable<Try<E>, E> transformer);
 
     /**
-     * Applique un filtre sur la valeur encapsulee et si celui-ci est valide; execute et retourne le résultat du transformateur
-     * @param predicate Contrainte à appliquer sur la valeur de l'instance courante
-     * @param transformer Lambda de transformation (retourne une instance de TRY), les exceptions sont gérées
-     * @return Valeur encapsulé de la lambda de transformation, l'instance courante qui est une erreur ou bien l'erreur encontree au sein de la lambda de transformation
+     * Applique un filtre sur l'instance et si celui-ci est valide; execute et retourne la valeur résultant de la transformation
+     *
+     * @param predicate   Contrainte à appliquer sur l'instance courante
+     * @param transformer Lambda de transformation (retourne une instance de ITRY), les exceptions sont gérées
+     *
+     * @return le résultat de la lambda de transformation (valeur ou erreur) ou bien l'instance courante
      */
-    Try<E> flatWhen(Predicate<E> predicate, FunctionThrowable<E, Try<E>> transformer);
+    Try<E> flatWhen(Predicate<Try<E>> predicate, FunctionThrowable<Try<E>, Try<E>> transformer);
 
     /**
-     * Exécute une lambda, seulement s'il s'agit d'un Try vide
-     * @param getter Lambda de récupération, les exceptions sont gérées
-     * @param <O> Type de sortie
-     * @return
-     */
-    <O> Try<O> whenIsEmpty(SupplierThrowable<O> getter);
-
-    /**
-     * Exécute une lambda, seulement s'il s'agit d'un Try vide
-     * @param getter Lambda de récupération retournant un Try, les exceptions sont gérées
-     * @param <O> Type de sortie
-     * @return
-     */
-    <O> Try<O> flatWhenIsEmpty(SupplierThrowable<Try<O>> getter);
-
-    /**
-     * Applique une transformation à la valeur encapsulé par l'objet Try, seulement s'il s'agit d'un Try de type SUCCESS
+     * Applique un filtre sur l'instance et si celui-ci est valide; execute et retourne la valeur résultant de la transformation.
+     * <br>
+     * Si l'instance ne correspond pas au filtre, l'instance retournée est soit vide, soit l'erreur
+     *
+     * @param predicate   Contrainte à appliquer sur l'instance courante
      * @param transformer Lambda de transformation, les exceptions sont gérées
-     * @param <O> Type de sortie
-     * @return
+     *
+     * @return le résultat de la lambda de transformation (valeur ou erreur) ou bien l'instance calculée
      */
-    <O> Try<O> whenIsSuccess(FunctionThrowable<E, O> transformer);
+    <O> Try<O> mapWhen(Predicate<Try<E>> predicate, FunctionThrowable<Try<E>, O> transformer);
+
 
     /**
-     * Applique une transformation à la valeur encapsulé par l'objet Try, seulement s'il s'agit d'un Try de type SUCCESS
-     * @param transformer Lambda de transformation retournant elle même un Try, les exceptions sont gérées
-     * @param <O> Type de sortie
-     * @return
+     * Applique un filtre sur l'instance et si celui-ci est valide; execute et retourne la valeur résultant de la transformation
+     * <br>
+     * Si l'instance ne correspond pas au filtre, l'instance retournée est soit vide, soit l'erreur
+     *
+     * @param predicate   Contrainte à appliquer sur l'instance courante
+     * @param transformer Lambda de transformation (retourne une instance de ITRY), les exceptions sont gérées
+     *
+     * @return le résultat de la lambda de transformation (valeur ou erreur) ou bien l'instance calculée
      */
-    <O> Try<O> flatWhenIsSuccess(FunctionThrowable<E, Try<O>> transformer);
-
-    /**
-     * Applique une transformation à la valeur encapsulé par l'objet Try, seulement s'il s'agit d'un Try de type SUCCESS /!\ ET NON EMPTY /!\
-     * @param transformer Lambda de transformation, les exceptions sont gérées
-     * @param <O> Type de sortie
-     * @return
-     */
-    <O> Try<O> whenIsSuccessStrict(FunctionThrowable<E, O> transformer);
-
-    /**
-     * Applique une transformation à la valeur encapsulé par l'objet Try, seulement s'il s'agit d'un Try de type SUCCESS /!\ ET NON EMPTY /!\
-     * @param transformer Lambda de transformation retournant elle même un Try, les exceptions sont gérées
-     * @param <O> Type de sortie
-     * @return
-     */
-    <O> Try<O> flatWhenIsSuccessStrict(FunctionThrowable<E, Try<O>> transformer);
-
-    /**
-     * Applique une transformation à la valeur encapsulé par l'objet Try, seulement s'il s'agit d'un Try de type FAILURE
-     * @param transformer Lambda de transformation, les exceptions sont gérées
-     * @param <O> Type de sortie
-     * @return
-     */
-    <O> Try<O> whenIsFailure(Function<Exception, O> transformer);
-
-    /**
-     * Applique une transformation à la valeur encapsulé par l'objet Try, seulement s'il s'agit d'un Try de type FAILURE
-     * @param transformer Lambda de transformation, les exceptions sont gérées
-     * @param <O> Type de sortie
-     * @return
-     */
-    <O> Try<O> flatWhenIsFailure(Function<Exception, Try<O>> transformer);
-
+    <O> Try<O> mapFlatWhen(Predicate<Try<E>> predicate, FunctionThrowable<Try<E>, Try<O>> transformer);
 }
--- src/main/java/com/java/common/functional/impl/TryImpl.java
+++ src/main/java/com/java/common/functional/impl/TryImpl.java
@@ -16,159 +16,128 @@ import java.util.function.Predicate;
  * A instance of a Try represents an attempt to compute a value. A Try is either a success, either a failure.
  * @param <E>
  */
-public abstract class TryImpl<E> implements Try<E> {
+ public abstract class TryImpl<E> implements Try<E> {
 
 
-    @Override
-    public boolean isEmpty() {
-        return TryFactory.empty().equals(this);
+     @Override
+     public boolean isEmpty() {
+         return TryFactory.empty().equals(this);
+     }
+
+     @Override
+     public boolean isSuccess() {
+         return getType() == TryType.SUCCESS;
+     }
+
+     @Override
+     public boolean isFailure() {
+         return getType() == TryType.FAILURE;
+     }
+
+     @Override
+     public Success<E> asSuccess() {
+         return (Success<E>) this;
+     }
+
+     @Override
+     public Failure<E> asFailure() {
+         return (Failure<E>) this;
+     }
+
+     @Override
+     public Try<E> filter(Predicate<E> predicate) {
+         return isFailure() || predicate.test(asSuccess().getResult())
+             ? this
+             : TryFactory.empty();
+     }
+
+     @Override
+     public Try<E> when(Predicate<Try<E>> predicate, FunctionThrowable<Try<E>, E> transformer) {
+         return predicate.test(this)?
+             TryFactory.of(transformer::apply).apply(this):
+             this;
+     }
+
+     @Override
+     public Try<E> flatWhen(final Predicate<Try<E>> predicate, final FunctionThrowable<Try<E>, Try<E>> transformer) {
+         return internalFlatWhen(predicate, transformer);
     }
 
-    @Override
-    public boolean isSuccess() {
-        return getType() == TryType.SUCCESS;
-    }
+     @Override
+     public <O> Try<O> mapWhen(Predicate<Try<E>> predicate, FunctionThrowable<Try<E>, O> transformer) {
+         return predicate.test(this)?
+             TryFactory.of(transformer::apply).apply(this):
+             // fix: on evite le ClassCastException
+             !this.isSuccess()? (Try<O>) this: TryFactory.empty();
+     }
 
-    @Override
-    public boolean isFailure() {
-        return !isSuccess();
-    }
+     @Override
+     public <O> Try<O> mapFlatWhen(Predicate<Try<E>> predicate, FunctionThrowable<Try<E>, Try<O>> transformer) {
+         return internalFlatWhen(predicate, transformer);
+     }
 
-    @Override
-    public Success<E> asSuccess() {
-        return (Success<E>) this;
-    }
+     @Override
+     public Optional<E> toOption() {
+         return isSuccess() ? Optional.ofNullable(asSuccess().getResult()) : Optional.empty();
+     }
 
-    @Override
-    public Failure<E> asFailure() {
-        return (Failure<E>) this;
-    }
+     @Override
+     public E getOrThrow() throws Exception {
+         if(isFailure()) throw asFailure().getException();
+         return asSuccess().getResult();
+     }
 
-    @Override
-    public Try<E> filter(Predicate<E> predicate) {
-        return isFailure() || predicate.test(asSuccess().getResult())
-            ? this
-            : TryFactory.empty();
-    }
+     @Override
+     public <ET extends Throwable> E getOrThrow(final Function<Exception, ET> transformer) throws ET {
+         if(isFailure()) throw transformer.apply(asFailure().getException());
+         return asSuccess().getResult();
+     }
 
-    @Override
-    public Try<E> when(Predicate<E> predicate, FunctionThrowable<E, E> transformer) {
-        return isSuccess() && predicate.test(asSuccess().getResult())?
-            TryFactory.of(transformer::apply).apply(asSuccess().getResult()):
-            this;
-    }
+     @Override
+     public Optional<E> getOptionOrThrow() throws Exception {
+         return Optional.ofNullable(getOrThrow());
+     }
 
-    @Override
-    public Try<E> flatWhen(Predicate<E> predicate, FunctionThrowable<E, Try<E>> transformer) {
-        Try<Try<E>> result = isSuccess() && predicate.test(asSuccess().getResult())?
-            TryFactory.of(transformer::apply).apply(asSuccess().getResult()):
-            (Try<Try<E>>) this;
-        // fix pour les retours Try.empty()
-        return result.isSuccess() && result.asSuccess().isTryResult()
-            ? result.asSuccess().getResult()
-            : (Try<E>) result;
-    }
+     @Override
+     public <ET extends Throwable> Optional<E> getOptionOrThrow(final Function<Exception, ET> transformer) throws ET {
+         return Optional.ofNullable(getOrThrow(transformer));
+     }
 
-    @Override
-    public <O> Try<O> whenIsEmpty(SupplierThrowable<O> getter) {
-        return isEmpty()? TryFactory.of(getter).get() : (Try<O>) this;
-    }
+     @Override
+     public E getOrElse(E defaultValue) {
+     	return isSuccess()? asSuccess().getResult(): defaultValue;
+     }
 
-    @Override
-    public <O> Try<O> flatWhenIsEmpty(SupplierThrowable<Try<O>> getter) {
-    	if (!isEmpty()) return (Try<O>) this;
-    	Try<Try<O>> result = whenIsEmpty(getter);
-        // fix pour les retours Try.empty()
-        return result.isSuccess() && result.asSuccess().isTryResult()
-    		? result.asSuccess().getResult()
-    		: (Try<O>) result;
-    }
+     @Override
+     public void ifPresent(Consumer<E> consumer) {
+         if(isSuccess()) consumer.accept(asSuccess().getResult());
+     }
 
-    @Override
-    public <O> Try<O> whenIsSuccess(FunctionThrowable<E, O> transformer) {
-        return isSuccess()?
-            TryFactory.of(transformer::apply).apply(asSuccess().getResult()):
-            (Try<O>) this;
-    }
+     @Override
+     public Try<E> peekIfPresent(Consumer<E> consumer) {
+         ifPresent(consumer);
+         return this;
+     }
 
-    @Override
-    public <O> Try<O> flatWhenIsSuccess(FunctionThrowable<E, Try<O>> transformer) {
-        Try<Try<O>> result = whenIsSuccess(transformer);
-        // fix pour les retours Try.empty()
-        return result.isSuccess() && result.asSuccess().isTryResult()
-    		? result.asSuccess().getResult()
-    		: (Try<O>) result;
-    }
+     @Override
+     public void ifAbsent(Consumer<Exception> consumer) {
+         if (isFailure()) consumer.accept(asFailure().getException());
+     }
 
-    @Override
-    public <O> Try<O> whenIsSuccessStrict(FunctionThrowable<E, O> transformer) {
-        return isSuccess() && !isEmpty()? TryFactory.of(transformer::apply).apply(asSuccess().getResult()) : (Try<O>) this;
-    }
+     @Override
+     public Try<E> peekIfAbsent(Consumer<Exception> consumer) {
+         ifAbsent(consumer);
+         return this;
+     }
 
-    @Override
-    public <O> Try<O> flatWhenIsSuccessStrict(FunctionThrowable<E, Try<O>> transformer) {
-        Try<Try<O>> result = whenIsSuccessStrict(transformer);
-        // fix pour les retours Try.empty()
-        return result.isSuccess() && result.asSuccess().isTryResult()
-    		? result.asSuccess().getResult()
-    		: (Try<O>) result;
-    }
-
-    @Override
-    public <O> Try<O> whenIsFailure(Function<Exception, O> transformer) {
-    	return isFailure() ? TryFactory.of(transformer::apply).apply(asFailure().getException()) : (Try<O>) this;
-    }
-    
-    @Override
-    public <O> Try<O> flatWhenIsFailure(Function<Exception, Try<O>> transformer) {
-        Try<Try<O>> result = whenIsFailure(transformer);
-        // fix pour les retours Try.empty()
-        return result.isSuccess() && result.asSuccess().isTryResult()
-    		? result.asSuccess().getResult()
-    		: (Try<O>) result;
-    }
-
-    @Override
-    public Optional<E> toOption() {
-        return isSuccess() ? Optional.ofNullable(asSuccess().getResult()) : Optional.empty();
-    }
-
-    @Override
-    public E getOrThrow() throws Exception {
-        if(isFailure()) throw asFailure().getException();
-        return asSuccess().getResult();
-    }
-
-    @Override
-    public Optional<E> getOptionOrThrow() throws Exception {
-        return Optional.ofNullable(getOrThrow());
-    }
-
-    @Override
-    public E getOrElse(E defaultValue) {
-    	return isSuccess()? asSuccess().getResult(): defaultValue; 
-    }
-    
-    @Override
-    public void ifPresent(Consumer<E> consumer) {
-        if(isSuccess()) consumer.accept(asSuccess().getResult());
-    }
-    
-    @Override
-    public Try<E> peekIfPresent(Consumer<E> consumer) {
-        ifPresent(consumer);
-        return this;
-    }
-
-    @Override
-    public void ifAbsent(Consumer<Exception> consumer) {
-        if (isFailure()) consumer.accept(asFailure().getException());
-    }
-
-    @Override
-    public Try<E> peekIfAbsent(Consumer<Exception> consumer) {
-        ifAbsent(consumer);
-        return this;
-    }
-
-}
+     private <O> Try<O> internalFlatWhen(Predicate<Try<E>> predicate, FunctionThrowable<Try<E>, Try<O>> transformer) {
+         Try<Try<O>> result = predicate.test(this)?
+             TryFactory.of(transformer::apply).apply(this):
+             // fix: on evite le ClassCastException
+             !this.isSuccess()? (Try<Try<O>>) this: TryFactory.empty();
+         // fix pour les retours Try.empty()
+         return result.isSuccess() && result.asSuccess().isTryResult()
+             ? result.asSuccess().getResult()
+             : (Try<O>) result;
+     }
+ }
